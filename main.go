f38448f6eacfec390df027e2d0959ca27d9c73ad
Kenny Cheng <kennycheng@comp.hkbu.edu.hk>
2021-05-18 19:50:26 +0800

    modified to use ReverseProxy in net/http/httputil

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-    "github.com/AaronO/gogo-proxy"
     "net/http"
+    "net/http/httputil"
+    "net/url"
     "os"
 )
 
 func main() {
-    p, _ := proxy.New(proxy.ProxyOptions{
-        Balancer: func(req *http.Request) (string, error) {
-            return os.Getenv("UPSTREAM_SERVER"), nil
-        },
-    })
-
-    http.ListenAndServe(":"+os.Getenv("PORT"), p)
+    u, _ := url.Parse(os.Getenv("UPSTREAM_SERVER"))
+    http.Handle("/", httputil.NewSingleHostReverseProxy(u))
+ 
+    // Start the server
+    http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
